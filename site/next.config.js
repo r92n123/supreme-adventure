4415d69964cd14b83b88b861812cd7758d19e560
r92n123 <148860731+r92n123@users.noreply.github.com>
2024-02-05 00:59:04 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,10 +2,10 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
+    name: 'Rohan Technologies',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://r92n123.kasmweb.com/supreme-adventure',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
